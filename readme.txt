fa5c2b476d97a1a4e414cdcb7f815a349ada2cfe
Gregory K <gregory@tiv.net>
2015-04-08 14:08:03 -0400

    Initial commits

---
+++ readme.txt
@@ -0,0 +1,54 @@
+=== WPGlobus Featured Images ===
+Contributors: alexgff, tivnetinc, tivnet
+Donate link: http://www.wpglobus.com/
+Tags: featured image, bilingual, globalization, i18n, international, l10n, language, localization, multilanguage, multilingual, switcher, translate, translation, WPGlobus
+Requires at least: 4.0
+Tested up to: 4.2
+Stable tag: trunk
+License: GPLv2
+License URI: https://github.com/WPGlobus/wpglobus-featured-images/blob/master/LICENSE
+
+== Description ==
+
+**WPGlobus Featured Images** is an extension to the WPGlobus plugin. It allows setting featured images separately for each language.
+
+> **NOTE:** You need to install and activate the [WPGlobus Multilingual Plugin](https://wordpress.org/plugins/wpglobus/) before installing the WPGlobus Featured Images extension.
+
+= More info =
+
+* [WPGlobus Featured Images home page](http://www.wpglobus.com/featured-images/).
+* [GitHub code repository](https://github.com/WPGlobus/wpglobus-featured-images).
+
+== Installation ==
+
+You can install this plugin directly from your WordPress dashboard:
+
+1. Go to the *Plugins* menu and click *Add New*.
+1. Search for *WPGlobus*.
+1. Click *Install Now* next to the WPGlobus plugin.
+1. Activate the plugin.
+
+Alternatively, see the guide to [Manually Installing Plugins](http://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation).
+
+== Frequently Asked Questions ==
+
+= How do I contribute to WPGlobus Featured Images? =
+
+We appreciate all contributions, ideas, critique, and help.
+
+* To speed up our development, please report bugs, with reproduction steps on [WPGlobus Featured Images GitHub](https://github.com/WPGlobus/wpglobus-featured-images).
+* Plugin and theme authors: please try WPGlobus Featured Images and let us know if you find any compatibility problems.
+* Contact us directly on [WPGlobus.com](http://www.wpglobus.com/contact-us/).
+
+= More info? =
+
+Please check out the [WPGlobus Website](http://www.wpglobus.com/featured-images) for additional information.
+
+== Screenshots ==
+
+1. Metabox interface.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
---
+++ wpglobus-featured-images.js
@@ -0,0 +1,109 @@
+/**
+ * WPGlobus Featured Images
+ * Interface JS functions
+ *
+ * @since 1.0.0
+ *
+ * @package WPGlobus Featured Images
+ * @subpackage Administration
+ */
+/*jslint browser: true*/
+/*global jQuery, console, WPGlobusCore, WPGlobusCoreData*/
+var WPGlobusFeaturedImages;
+
+jQuery(document).ready(function($) {
+
+	var api;
+	api = WPGlobusFeaturedImages = {
+		start : true,
+		action : '',
+		language : '',
+		default_language : '',
+		win : '',
+		wp_native_action : false, 
+		init : function() {
+			try {
+				$('#wpglobus-featured-images-tabs').tabs();
+				
+			} catch (exception) {api.start=false}
+			
+			api.win = window.dialogArguments || opener || parent || top;
+			if ( 'undefined' != typeof api.win.WPGlobusCoreData ) {
+				api.default_language = api.win.WPGlobusCoreData.default_language;	
+				$('#postimagediv').css({'display':'none'});
+				$('#postimagediv-hide').prop('checked',false);
+				this.attachListener();
+			}	
+		},
+		removeThumbnail : function(nonce, language, post_id) {
+			api.wp_native_action = false;
+			var order = {};
+			order['action']   = 'wpglobus-remove-post-thumbnail';
+			order['language']  = language;
+			order['attr'] 	  = {};		
+			order['attr']['post_id'] = post_id;		
+			order['attr']['thumbnail_id'] = -1;		
+		
+			api.ajax(order);			
+		},	
+		setThumbnailHTML : function(html, language){
+			$('#featured-images-tab-'+language).html(html);
+		},
+		setLanguage : function() {
+			return $('#wpglobus-featured-images-tabs').data('featured-image-language');
+		},
+		ajax : function(order) {
+
+			$.ajax({type:'POST', url:WPGlobusFImages.ajaxurl, data:{action:WPGlobusFImages.process_ajax, order:order}, dataType:'json'})
+			.done(function (result) {
+				if ( result['result'] == 'ok' ) {
+					api.win.WPGlobusFeaturedImages.setThumbnailHTML(result['html'], order['language']);
+				}	
+			})
+			.fail(function (error){})
+			.always(function (jqXHR, status){});		
+		},	
+		attachListener : function() {
+			$('#wpglobus_postimagediv').on( 'click', '.wpglobus-set-post-thumbnail', function( event ) {
+				event.preventDefault();
+				// Stop propagation to prevent thickbox from activating.
+				event.stopPropagation();
+				api.language = $(this).data('language');	
+				wp.media.featuredImage.frame().open();
+			});			
+			
+			$('#set-post-thumbnail').on('click', function(event){
+				api.wp_native_action = true;
+			});
+			
+			$(document).ajaxSend(function(event, jqxhr, settings){
+				if ( settings.data.indexOf('action=set-post-thumbnail') >= 0 && ! api.wp_native_action ) {
+					
+					if ( settings.data.indexOf('thumbnail_id=-1') >= 0 ) {
+						return;	
+					}
+					jqxhr.abort();
+					var s=settings.data.split('&'),
+						attr = {};
+					$.each(s, function(i,e){
+						var ss = e.split('=');
+						attr[ss[0]] = {};
+						attr[ss[0]] = ss[1];
+					});
+					var order = {};
+					order['action']   = 'wpglobus-set-post-thumbnail';
+					order['language']  = api.language;
+					order['attr'] 	  = attr;	
+			
+					$('a#wp-post-thumbnail-' + attr['thumbnail_id']).fadeOut(2000);
+					api.ajax(order);
+				}
+			});
+			
+		}	
+	
+	};
+
+	WPGlobusFeaturedImages.init();
+
+});
