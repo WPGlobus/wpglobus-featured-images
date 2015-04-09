2076be684e9c9304039fb41250444fd7a3ec192d
Gregory K <gregory@tiv.net>
2015-04-08 21:40:55 -0400

    General code cleanup

--- wpglobus-featured-images.php
+++ wpglobus-featured-images.php
@@ -31,14 +31,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 define( 'WPGLOBUS_FEATURED_IMAGES_VERSION', '1.0.0' );
 
-add_action( 'plugins_loaded' , 'wpglobus_featured_images_load' , 11 );
+add_action( 'plugins_loaded', 'wpglobus_featured_images_load', 11 );
 function wpglobus_featured_images_load() {
-	if ( class_exists('WPGlobus') && 'off' != WPGlobus::Config()->toggle ) {
+	if ( class_exists( 'WPGlobus' ) && 'off' != WPGlobus::Config()->toggle ) {
 		new WPGlobus_Featured_Images();
-	}	
-}	
+	}
+}
 
-if ( ! class_exists('WPGlobus_Featured_Images') ) :
+if ( ! class_exists( 'WPGlobus_Featured_Images' ) ) :
 
 	/**
 	 * WPGlobus_Featured_Images
@@ -47,174 +47,181 @@ if ( ! class_exists('WPGlobus_Featured_Images') ) :
 
 		/** */
 		function __construct() {
-			
+
 			if ( is_admin() ) {
-				
+
 				add_action( 'admin_print_scripts', array(
 					$this,
-					'on_admin_scripts' 
+					'on_admin_scripts'
 				) );
-				
+
 				add_action( 'add_meta_boxes', array(
 					$this,
-					'on_add_meta_boxes' 
+					'on_add_meta_boxes'
 				) );
 
 				add_action( 'wp_ajax_' . __CLASS__ . '_process_ajax', array(
-					$this, 
-					'on_process_ajax' 
+					$this,
+					'on_process_ajax'
 				) );
-				
+
 			} else {
 
 				add_filter( 'post_thumbnail_html', array(
 					$this,
-					'on_post_thumbnail_html' 
+					'on_post_thumbnail_html'
 				), 10, 5 );
 
-			}	
-			
-		}	
-		
+			}
+
+		}
+
 		/**
 		 * Handle ajax process
-		 * 
 		 * @since 1.0.0
 		 */
 		function on_process_ajax() {
-			
+
 			$ajax_return = array();
 
 			$order = $_POST['order'];
 
 			switch ( $order['action'] ) :
 				case 'wpglobus-remove-post-thumbnail':
-				
-					$ajax_return['action']  = $order['action'];
+
+					$ajax_return['action'] = $order['action'];
 					if ( $order['language'] == WPGlobus::Config()->default_language ) {
 
 						if ( delete_post_meta( $order['attr']['post_id'], '_thumbnail_id' ) ) {
-							$ajax_return['result']  = 'ok';
-							$ajax_return['html'] 	= $this->_post_thumbnail_html( null, get_post($order['attr']['post_id']), $order['language']  );
+							$ajax_return['result'] = 'ok';
+							$ajax_return['html']   =
+								$this->_post_thumbnail_html( null, get_post( $order['attr']['post_id'] ), $order['language'] );
 						} else {
 							$ajax_return['html'] = 'Error';
 						}
-						
+
 					} else {
 
-						$wpglobus_thumbnail_ids = (array) get_post_meta( $order['attr']['post_id'], 'wpglobus_thumbnail_ids', true );
-						
-					
-						if ( ! empty($wpglobus_thumbnail_ids[$order['language']]) ) {
-							
-							unset( $wpglobus_thumbnail_ids[$order['language']] );
-							
+						$wpglobus_thumbnail_ids =
+							(array) get_post_meta( $order['attr']['post_id'], 'wpglobus_thumbnail_ids', true );
+
+
+						if ( ! empty( $wpglobus_thumbnail_ids[ $order['language'] ] ) ) {
+
+							unset( $wpglobus_thumbnail_ids[ $order['language'] ] );
+
 							if ( update_post_meta( $order['attr']['post_id'], 'wpglobus_thumbnail_ids', $wpglobus_thumbnail_ids ) ) {
-							
-								$ajax_return['result']  = 'ok';
-								$ajax_return['html'] 	= $this->_post_thumbnail_html( null, get_post($order['attr']['post_id']), $order['language']  );
-									
-							
-							}
-						
-						}	
-			
-					}	
-				
-				break;
-				case 'wpglobus-set-post-thumbnail':
-				
-					$ajax_return['action']  = $order['action'];
-					
-					if ( $order['language'] == WPGlobus::Config()->default_language ) {
-						
-						if ( update_post_meta( $order['attr']['post_id'], '_thumbnail_id', $order['attr']['thumbnail_id'] ) ) {
-							$ajax_return['result']  = 'ok';
-							$ajax_return['html'] 	= $this->_post_thumbnail_html( $order['attr']['thumbnail_id'], get_post($order['attr']['post_id']), $order['language']  );
-						} else {
-							$ajax_return['result'] = 'error';
-							$ajax_return['html'] 	= '<p>Error</p>';
-						}	
-						
-					} else {
-					
-						$wpglobus_thumbnail_ids = (array) get_post_meta( $order['attr']['post_id'], 'wpglobus_thumbnail_ids', true );
-						
-						$wpglobus_thumbnail_ids[$order['language']] = $order['attr']['thumbnail_id'];
-						
-						if ( update_post_meta( $order['attr']['post_id'], 'wpglobus_thumbnail_ids', $wpglobus_thumbnail_ids ) ) {
-							$ajax_return['result']  = 'ok';
-							$ajax_return['html'] 	= $this->_post_thumbnail_html( $order['attr']['thumbnail_id'], get_post($order['attr']['post_id']), $order['language']  );
-						} else {
-							$ajax_return['result'] = 'error';
-							$ajax_return['html'] 	= '<p>Error</p>';
-						}						
 
-					}	
-				break;
+								$ajax_return['result'] = 'ok';
+								$ajax_return['html']   =
+									$this->_post_thumbnail_html( null, get_post( $order['attr']['post_id'] ), $order['language'] );
+
+
+							}
+
+						}
+
+					}
+
+					break;
+				case 'wpglobus-set-post-thumbnail':
+
+					$ajax_return['action'] = $order['action'];
+
+					if ( $order['language'] == WPGlobus::Config()->default_language ) {
+
+						if ( update_post_meta( $order['attr']['post_id'], '_thumbnail_id', $order['attr']['thumbnail_id'] ) ) {
+							$ajax_return['result'] = 'ok';
+							$ajax_return['html']   =
+								$this->_post_thumbnail_html( $order['attr']['thumbnail_id'], get_post( $order['attr']['post_id'] ), $order['language'] );
+						} else {
+							$ajax_return['result'] = 'error';
+							$ajax_return['html']   = '<p>Error</p>';
+						}
+
+					} else {
+
+						$wpglobus_thumbnail_ids =
+							(array) get_post_meta( $order['attr']['post_id'], 'wpglobus_thumbnail_ids', true );
+
+						$wpglobus_thumbnail_ids[ $order['language'] ] = $order['attr']['thumbnail_id'];
+
+						if ( update_post_meta( $order['attr']['post_id'], 'wpglobus_thumbnail_ids', $wpglobus_thumbnail_ids ) ) {
+							$ajax_return['result'] = 'ok';
+							$ajax_return['html']   =
+								$this->_post_thumbnail_html( $order['attr']['thumbnail_id'], get_post( $order['attr']['post_id'] ), $order['language'] );
+						} else {
+							$ajax_return['result'] = 'error';
+							$ajax_return['html']   = '<p>Error</p>';
+						}
+
+					}
+					break;
 			endswitch;
-			
+
 			echo json_encode( $ajax_return );
 			die();
 		}
-		
+
 		/**
 		 * Retrieve html for thumbnail at front-end.
-		 * 
 		 * @since 1.0.0
-		 * @see post_thumbnail_html filter
-         * @var string $html
-         * @var int $post_id
-         * @var int $post_thumbnail_id
-         * @var array $size
-         * @var array $attr
+		 * @see   post_thumbnail_html filter
+		 * @var string $html
+		 * @var int    $post_id
+		 * @var int    $post_thumbnail_id
+		 * @var array  $size
+		 * @var array  $attr
 		 * @return string html
-		 */	
-		function on_post_thumbnail_html( $html, $post_id,
+		 */
+		function on_post_thumbnail_html(
+			$html, $post_id,
 			/** @noinspection PhpUnusedParameterInspection */
 			$post_thumbnail_id,
-			$size, $attr ) {
+			$size, $attr
+		) {
 
+			/** @global WP_Post $post */
 			global $post;
 			$post_type = empty( $post ) ? '' : $post->post_type;
-			if ( empty($post_type) ) {
+			if ( empty( $post_type ) ) {
 				return '';
 			}
-			
-			if ( !empty(WPGlobus::Config()->disabled_entities) && in_array($post_type, WPGlobus::Config()->disabled_entities) ) {
+
+			if ( ! empty( WPGlobus::Config()->disabled_entities ) && in_array( $post_type, WPGlobus::Config()->disabled_entities ) ) {
 				return '';
 			}
-			
+
 			if ( WPGlobus::Config()->language != WPGlobus::Config()->default_language ) {
 				$wpglobus_thumbnail_ids = (array) get_post_meta( $post_id, 'wpglobus_thumbnail_ids', true );
-				if ( !empty($wpglobus_thumbnail_ids[WPGlobus::Config()->language]) ) {
-					$html = wp_get_attachment_image( $wpglobus_thumbnail_ids[WPGlobus::Config()->language], $size, false, $attr );
+				if ( ! empty( $wpglobus_thumbnail_ids[ WPGlobus::Config()->language ] ) ) {
+					$html =
+						wp_get_attachment_image( $wpglobus_thumbnail_ids[ WPGlobus::Config()->language ], $size, false, $attr );
 				}
-			}	
-		
+			}
+
 			return $html;
-			
+
 		}
-			
+
 		/**
 		 * Enqueue admin scripts
-		 *
 		 * @since 1.0.0
-		 *
 		 * @return void
 		 */
 		function on_admin_scripts() {
 
+			/** @global WP_Post $post */
 			global $post;
 			$post_type = empty( $post ) ? '' : $post->post_type;
-			if ( empty($post_type) ) {
+			if ( empty( $post_type ) ) {
 				return;
 			}
-			
-			if ( !empty(WPGlobus::Config()->disabled_entities) && in_array($post_type, WPGlobus::Config()->disabled_entities) ) {
+
+			if ( ! empty( WPGlobus::Config()->disabled_entities ) && in_array( $post_type, WPGlobus::Config()->disabled_entities ) ) {
 				return;
 			}
-			
+
 			wp_register_script(
 				'wpglobus-featured-images',
 				plugins_url() . "/wpglobus-featured-images/wpglobus-featured-images.js",
@@ -227,7 +234,7 @@ if ( ! class_exists('WPGlobus_Featured_Images') ) :
 				'wpglobus-featured-images',
 				'WPGlobusFImages',
 				array(
-					'version' => '',
+					'version'      => '',
 					'ajaxurl'      => admin_url( 'admin-ajax.php' ),
 					'parentClass'  => __CLASS__,
 					'process_ajax' => __CLASS__ . '_process_ajax'
@@ -237,173 +244,181 @@ if ( ! class_exists('WPGlobus_Featured_Images') ) :
 
 		/**
 		 * Add meta box
-		 *
 		 * @since 1.0.0
-		 * 
 		 * @return void
 		 */
 		function on_add_meta_boxes() {
-		
+
+			/** @global WP_Post $post */
 			global $post;
 			$post_type = empty( $post ) ? '' : $post->post_type;
-			if ( empty($post_type) ) {
+			if ( empty( $post_type ) ) {
 				return;
 			}
-			
-			if ( !empty(WPGlobus::Config()->disabled_entities) && in_array($post_type, WPGlobus::Config()->disabled_entities) ) {
+
+			if ( ! empty( WPGlobus::Config()->disabled_entities ) && in_array( $post_type, WPGlobus::Config()->disabled_entities ) ) {
 				return;
 			}
-		
+
+			/** @global string $pagenow */
 			global $pagenow;
-			
+
 			if ( $pagenow == 'post.php' ) :
 
-				$thumbnail_support = current_theme_supports( 'post-thumbnails', $post_type ) && post_type_supports( $post_type, 'thumbnail' );
+				$thumbnail_support =
+					current_theme_supports( 'post-thumbnails', $post_type ) && post_type_supports( $post_type, 'thumbnail' );
 				if ( ! $thumbnail_support && 'attachment' === $post_type && $post->post_mime_type ) {
 					if ( 0 === strpos( $post->post_mime_type, 'audio/' ) ) {
-						$thumbnail_support = post_type_supports( 'attachment:audio', 'thumbnail' ) || current_theme_supports( 'post-thumbnails', 'attachment:audio' );
+						$thumbnail_support =
+							post_type_supports( 'attachment:audio', 'thumbnail' ) || current_theme_supports( 'post-thumbnails', 'attachment:audio' );
 					} elseif ( 0 === strpos( $post->post_mime_type, 'video/' ) ) {
-						$thumbnail_support = post_type_supports( 'attachment:video', 'thumbnail' ) || current_theme_supports( 'post-thumbnails', 'attachment:video' );
+						$thumbnail_support =
+							post_type_supports( 'attachment:video', 'thumbnail' ) || current_theme_supports( 'post-thumbnails', 'attachment:video' );
 					}
 				}
 
 				if ( $thumbnail_support && current_user_can( 'upload_files' ) ) {
 					add_meta_box(
-						'wpglobus_postimagediv', 
-						__('WPGlobus Featured Images'),
-						array( $this, 'post_thumbnail_meta_box' ), 
-						null, 
-						'side', 
+						'wpglobus_postimagediv',
+						__( 'WPGlobus Featured Images' ),
+						array( $this, 'post_thumbnail_meta_box' ),
+						null,
+						'side',
 						'low'
 					);
 				}
 
 			endif;
-			
+
 		}
 
 		/**
 		 * Display post thumbnail meta box.
-		 *
 		 * @since 1.0.0
-		 * 
 		 * @return void
 		 */
 		function post_thumbnail_meta_box() {
+			/** @global WP_Post $post */
 			global $post;
-			
-			$thumbnail_id = get_post_meta( $post->ID, '_thumbnail_id', true );
+
+			$thumbnail_id           = get_post_meta( $post->ID, '_thumbnail_id', true );
 			$wpglobus_thumbnail_ids = (array) get_post_meta( $post->ID, 'wpglobus_thumbnail_ids', true );
-			
+
 			echo $this->featured_images_tabs( $thumbnail_id, $wpglobus_thumbnail_ids, $post->ID );
-			
+
 		}
 
 		/**
 		 * Output Tabs for the post thumbnail meta-box.
-		 *
 		 * @since 1.0.0
 		 *
-		 * @param int $thumbnail_id_default_language ID of the attachment used for thumbnail
-		 * @param array $wpglobus_thumbnail_ids The IDs of attachment used for language thumbnail
-		 * @param mixed $post The post ID or object associated with the thumbnail, defaults to global $post.
+		 * @param int   $thumbnail_id_default_language ID of the attachment used for thumbnail
+		 * @param array $wpglobus_thumbnail_ids        The IDs of attachment used for language thumbnail
+		 * @param mixed $post                          The post ID or object associated with the thumbnail, defaults to global $post.
+		 *
 		 * @return string html
 		 */
 		function featured_images_tabs( $thumbnail_id_default_language = null, $wpglobus_thumbnail_ids = null, $post = null ) {
 
-				/** @global $post */
-				global $post;
+			/** @global WP_Post $post */
+			global $post;
 
-				?>
+			?>
 
-				<div id="wpglobus-featured-images-tabs" class="wpglobus-post-body-tabs" data-featured-image-language="">
-					<ul class="wpglobus-featured-images-list">    <?php
-						foreach ( WPGlobus::Config()->open_languages as $language ) { ?>
-							<li id="featured-images-link-tab-<?php echo $language; ?>"
-								data-language="<?php echo $language; ?>"
-								class="wpglobus-featured-images-tab"><a
-									href="#featured-images-tab-<?php echo $language; ?>"><?php echo WPGlobus::Config()->en_language_name[ $language ]; ?></a>
-							</li> <?php
-						} ?>
-					</ul>    <?php
-
-					foreach ( WPGlobus::Config()->open_languages as $language ) {	?>
-						<div id="featured-images-tab-<?php echo $language; ?>" class="wpglobus-featured-images-general" 
-							data-language="<?php echo $language; ?>">
-							<?php 
-							if ( $language != WPGlobus::Config()->default_language ) {
-								
-								if ( !empty($wpglobus_thumbnail_ids[$language] ) ) {
-									$thumbnail_id = $wpglobus_thumbnail_ids[$language];
-								} else {
-									$thumbnail_id = null;
-								}	
-								
-							} else { 
-								$thumbnail_id = $thumbnail_id_default_language;
-							}
-						
-							echo $this->_post_thumbnail_html( $thumbnail_id, $post, $language );
-							?>
-						</div> <?php
+			<div id="wpglobus-featured-images-tabs" class="wpglobus-post-body-tabs" data-featured-image-language="">
+				<ul class="wpglobus-featured-images-list">    <?php
+					foreach ( WPGlobus::Config()->open_languages as $language ) { ?>
+						<li id="featured-images-link-tab-<?php echo $language; ?>"
+						    data-language="<?php echo $language; ?>"
+						    class="wpglobus-featured-images-tab"><a
+								href="#featured-images-tab-<?php echo $language; ?>"><?php echo WPGlobus::Config()->en_language_name[ $language ]; ?></a>
+						</li> <?php
 					} ?>
-				</div> <?php
+				</ul>    <?php
+
+				foreach ( WPGlobus::Config()->open_languages as $language ) { ?>
+					<div id="featured-images-tab-<?php echo $language; ?>" class="wpglobus-featured-images-general"
+					     data-language="<?php echo $language; ?>">
+						<?php
+						if ( $language != WPGlobus::Config()->default_language ) {
+
+							if ( ! empty( $wpglobus_thumbnail_ids[ $language ] ) ) {
+								$thumbnail_id = $wpglobus_thumbnail_ids[ $language ];
+							} else {
+								$thumbnail_id = null;
+							}
+
+						} else {
+							$thumbnail_id = $thumbnail_id_default_language;
+						}
+
+						echo $this->_post_thumbnail_html( $thumbnail_id, $post, $language );
+						?>
+					</div> <?php
+				} ?>
+			</div> <?php
 
 		}
 
 		/**
 		 * Retrieve html for thumbnail.
-		 *
 		 * @since 1.0.0
 		 *
-		 * @param int $thumbnail_id ID of the attachment used for thumbnail
-		 * @param mixed $post The post ID or object associated with the thumbnail, defaults to global $post.
+		 * @param int    $thumbnail_id ID of the attachment used for thumbnail
+		 * @param mixed  $post         The post ID or object associated with the thumbnail, defaults to global $post.
 		 * @param string $language
+		 *
 		 * @return string html
 		 */
 		function _post_thumbnail_html( $thumbnail_id = null, $post = null, $language ) {
-			
+
+			/** @global int $content_width */
+			/** @global array $_wp_additional_image_sizes */
 			global $content_width, $_wp_additional_image_sizes;
 
-			$upload_iframe_src = esc_url( get_upload_iframe_src('image', $post->ID ) ); 
+			$upload_iframe_src = esc_url( get_upload_iframe_src( 'image', $post->ID ) );
 
-			$upload_iframe_src = str_replace( 'type=image', 'type=image&language='.$language, $upload_iframe_src );
+			$upload_iframe_src = str_replace( 'type=image', 'type=image&language=' . $language, $upload_iframe_src );
 
-			$set_thumbnail_link = '<p style="clear:both;" class="hide-if-no-js"><a title="' . esc_attr__( 'Set featured image' ) . '" href="%s" id="set-post-thumbnail-' . $language . '" class="thickbox wpglobus-set-post-thumbnail" data-language="'.$language.'">%s</a></p>';
-			$content = sprintf( $set_thumbnail_link, $upload_iframe_src, esc_html__( 'Set featured image' ) );
+			$set_thumbnail_link =
+				'<p style="clear:both;" class="hide-if-no-js"><a title="' . esc_attr__( 'Set featured image' ) . '" href="%s" id="set-post-thumbnail-' . $language . '" class="thickbox wpglobus-set-post-thumbnail" data-language="' . $language . '">%s</a></p>';
+			$content            =
+				sprintf( $set_thumbnail_link, $upload_iframe_src, esc_html__( 'Set featured image' ) );
 
 			if ( $thumbnail_id && get_post( $thumbnail_id ) ) {
 				$old_content_width = $content_width;
-				$content_width = 220;  // 266
-				
-				if ( !isset( $_wp_additional_image_sizes['post-thumbnail'] ) )
+				$content_width     = 220;  // 266
+
+				if ( ! isset( $_wp_additional_image_sizes['post-thumbnail'] ) ) {
 					$thumbnail_html = wp_get_attachment_image( $thumbnail_id, array( $content_width, $content_width ) );
-				else
+				} else {
 					$thumbnail_html = wp_get_attachment_image( $thumbnail_id, 'post-thumbnail' );
-				
-				if ( !empty( $thumbnail_html ) ) {
+				}
+
+				if ( ! empty( $thumbnail_html ) ) {
 					$ajax_nonce = wp_create_nonce( 'set_post_thumbnail-' . $post->ID );
 
-					$onclick = "WPGlobusFeaturedImages.removeThumbnail('" . $ajax_nonce . "', '" . $language . "', '" . $post->ID . "');return false;";
+					$onclick =
+						"WPGlobusFeaturedImages.removeThumbnail('" . $ajax_nonce . "', '" . $language . "', '" . $post->ID . "');return false;";
 
 					$content = sprintf( $set_thumbnail_link, $upload_iframe_src, $thumbnail_html );
-					$content .= '<p class="hide-if-no-js"><a href="#" id="remove-post-thumbnail-'.$language.'" onclick="'.$onclick.'">' . esc_html__( 'Remove featured image' ) . '</a></p>';
+					$content .= '<p class="hide-if-no-js"><a href="#" id="remove-post-thumbnail-' . $language . '" onclick="' . $onclick . '">' . esc_html__( 'Remove featured image' ) . '</a></p>';
 				}
 				$content_width = $old_content_width;
 			}
 
 			/**
 			 * Filter the admin post thumbnail HTML markup to return.
-			 *
 			 * @since 1.0.0
 			 *
 			 * @param string $content Admin post thumbnail HTML markup.
 			 * @param int    $post_id Post ID.
 			 */
+
 			//return apply_filters( 'admin_post_thumbnail_html', $content, $post->ID );
-			
+
 			return $content;
-			
+
 		}
 
 	} // class
